ce73b88f502c85bb23721c47528f9e0b5a7a9b5f
florianbelkadi <belkadiflorian1@gmail.com>
2023-02-22 13:32:40 +0100

    PremierImport

---
+++ footer.php
@@ -0,0 +1,34 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="script.js"></script>
+
+
+
+</body>
+
+
+<footer>
+<div class="footer">
+<h2>Prévisions météo</h2>
+
+        <div class="prevision">
+        <div class="box">
+            <div>Demain</div>
+            <div class="soleilIcon"></div>
+            <div>Temp</div>
+        </div>
+        <div class="box">
+            <div>Demain</div>
+            <div class="orageIcon"></div>
+            <div>Temp</div>
+        </div>
+        <div class="box">
+            <div>Demain</div>
+            <div class="pluieIcon"></div>
+            <div>Temp</div>
+        </div>
+        </div>
+</div>
+</footer>
+
+
+</html>
